cf3a6c5fbee8b0918dac1bde9f708d5571bc1d62
Kaylee Dao <kayleed2@gmail.com>
2022-08-01 13:47:19 -0500

    Betty

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,13 +8,13 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    if (!tree)
-    {
-        return (0);
-    }
-    if (!tree->left && !tree->right)
-    {
-        return (0);
-    }
-   return (binary_tree_nodes(tree->right) + 1 + binary_tree_nodes(tree->left));
+if (!tree)
+{
+return (0);
+}
+if (!tree->left && !tree->right)
+{
+return (0);
+}
+return (binary_tree_nodes(tree->right) + 1 + binary_tree_nodes(tree->left));
 }
