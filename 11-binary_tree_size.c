5a1a64ad3fadb4456389a353203ec5fbcd4b80df
Kaylee Dao <kayleed2@gmail.com>
2022-08-01 13:27:15 -0500

    Size of each height

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Measures depth of node of binary tree
+ * @tree: pointer to  node of tree to measure size
+ * Return: Size or NULL
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+    if (!tree)
+    {
+        return (0);
+    }
+    return (binary_tree_size(tree->right) + 1 + binary_tree_size(tree->left));
+}
