4695c305e28d3c24ff0e7991c0c2c1c265f71b3f
Kaylee Dao <kayleed2@gmail.com>
2022-08-01 15:32:16 -0500

    Betty

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,21 +8,21 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    binary_tree_t *temp = NULL;
-    
-    if (!node || node->parent == NULL)
-    {
-        return (NULL);
-    }
+binary_tree_t *temp = NULL;
 
-    if (node->parent->left == node)
-    {
-        temp = node->parent->right;
-    }
-    else if (node->parent->right == node)
-    {
-        temp = node->parent->left;
-    }
-    
-    return (temp);
+if (!node || node->parent == NULL)
+{
+return (NULL);
+}
+
+if (node->parent->left == node)
+{
+temp = node->parent->right;
+}
+else if (node->parent->right == node)
+{
+temp = node->parent->left;
+}
+
+return (temp);
 }
