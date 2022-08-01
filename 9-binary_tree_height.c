e87dcbf2eedc9ffc531488830fa5daf896562575
Kaylee Dao <kayleed2@gmail.com>
2022-08-01 13:03:11 -0500

    Getting height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,6 +8,9 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+size_t left;
+size_t right;
+
 if (tree == NULL)
 {
 return (0);
@@ -18,8 +21,8 @@ if (!tree->left && !tree->right)
 return (0);
 }
 
-size_t left = binary_tree_height(tree->left);
-size_t right = binary_tree_height(tree->right);
+left = binary_tree_height(tree->left);
+right = binary_tree_height(tree->right);
 
 if (left >= right)
 {
