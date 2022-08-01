9ca1d7e9310d91a8ba6f8442d8c134db94964669
Kaylee Dao <kayleed2@gmail.com>
2022-08-01 10:57:51 -0500

    Use former function to set new nodes

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Creates tree node
+ * @parent: pointer to left node
+ * @value: Value to put in new node
+ * Return: Pointer to new node on success, NULL on failure
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    if (parent == NULL)
+    {
+        return (NULL);
+    }
+    
+    if (parent->left == NULL)
+    {
+        parent->left = binary_tree_node(parent, value);
+    }
+    else
+    {
+        parent->left->left =  binary_tree_node(parent->left, value);;
+    }
+    return (parent->left);
+}
