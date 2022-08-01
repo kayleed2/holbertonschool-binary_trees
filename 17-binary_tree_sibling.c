2fefd8dec6e56723c80fe4f93c57fb3979e2c1e2
Kaylee Dao <kayleed2@gmail.com>
2022-08-01 15:31:14 -0500

    Return of root

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - Checks if binary tree is full
+ * @node: pointer to node to find sibling
+ * Return: Pointer to sibling node or NULL
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+    binary_tree_t *temp = NULL;
+    
+    if (!node || node->parent == NULL)
+    {
+        return (NULL);
+    }
+
+    if (node->parent->left == node)
+    {
+        temp = node->parent->right;
+    }
+    else if (node->parent->right == node)
+    {
+        temp = node->parent->left;
+    }
+    
+    return (temp);
+}
