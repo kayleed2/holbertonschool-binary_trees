6acd983ae41122d0d57f5cfe4209ae0a20937b0a
Kaylee Dao <kayleed2@gmail.com>
2022-08-01 13:46:54 -0500

    Checking either side of node

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Countss nodes with one child
+ * @tree: pointer to root node
+ * Return: Size or NULL
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+    if (!tree)
+    {
+        return (0);
+    }
+    if (!tree->left && !tree->right)
+    {
+        return (0);
+    }
+   return (binary_tree_nodes(tree->right) + 1 + binary_tree_nodes(tree->left));
+}
