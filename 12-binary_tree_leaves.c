4e25416144869e20dc729dd55759baf6fb2b5104
Kaylee Dao <kayleed2@gmail.com>
2022-08-01 13:33:46 -0500

    Draft

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - Counts leaves of binary tree
+ * @tree: pointer to root node
+ * Return: Size or NULL
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+if (!tree)
+{
+return (0);
+}
+
+if (tree->left == NULL && tree->right == NULL)
+{
+return (1);
+}
+return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+}
