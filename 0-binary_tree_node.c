55758a8d83b98e721036bb2643f9e88c1026c6de
Kaylee Dao <kayleed2@gmail.com>
2022-08-01 10:07:16 -0500

    Betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,20 +7,23 @@
  * Return: Pointer to new node on success, NULL on failure
  */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
-    
-    binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
 
-    if (!newnode) {
-        return (NULL);
-    }
-    newnode->n = value;
-    newnode->left = NULL;
-    newnode->right = NULL;
+binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
 
-    if (!parent) {
-        newnode->parent = NULL;
-    }
-    newnode->parent = parent;
-    return (newnode);
+if (!newnode)
+{
+return (NULL);
+}
+newnode->n = value;
+newnode->left = NULL;
+newnode->right = NULL;
+
+if (!parent)
+{
+newnode->parent = NULL;
+}
+newnode->parent = parent;
+return (newnode);
 }
