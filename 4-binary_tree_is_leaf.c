0261bef9216a7a39be22b040c2c0cce0f8d2ed76
Kaylee Dao <kayleed2@gmail.com>
2022-08-01 11:28:40 -0500

    Checking for child nodes

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * is_leaf - Checks if node is leaf
+ * @node: pointer to node to check
+ * Return: 1 if node is leaf, else 0
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+    if (!node)
+    {
+        return(0);
+    }
+    if (node->left == NULL && node->right == NULL)
+    {
+        return(1);
+    }
+    return(0);
+}
