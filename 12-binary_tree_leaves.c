359ea91a696d91500ef9f3b72a66d59a95ace606
Kaylee Dao <kayleed2@gmail.com>
2022-08-01 13:35:29 -0500

    Betty

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,10 +12,9 @@ if (!tree)
 {
 return (0);
 }
-
 if (tree->left == NULL && tree->right == NULL)
 {
 return (1);
 }
-return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
