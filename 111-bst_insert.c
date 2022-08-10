5d64c2b3abdad718f57d1cc94a8b72989af1a903
Kaylee Dao <kayleed2@gmail.com>
2022-08-10 10:57:55 -0500

    Betty

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -9,36 +9,36 @@
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-     if (tree == NULL)
-    {
-        return (NULL);
-    }
+if (tree == NULL)
+{
+return (NULL);
+}
 
 
-    if (*tree == NULL)
-    {
-        *tree = binary_tree_node(*tree, value);
-        return (*tree);
-    }
+if (*tree == NULL)
+{
+*tree = binary_tree_node(*tree, value);
+return (*tree);
+}
 
-    if (value < (*tree)->n)
-    {
-        if ((*tree)->left == NULL)
-        {
-            (*tree)->left = binary_tree_node(*tree, value);
-            return ((*tree)->left);
-        }
-        return (bst_insert(&((*tree)->left), value));
-    }
-    if (value > (*tree)->n)
-    {
-        if ((*tree)->right == NULL)
-        {
-            (*tree)->right = binary_tree_node(*tree, value);
-            return ((*tree)->right);
-        }
-        return (bst_insert(&(*tree)->right, value));
-    }
-    return (NULL);
+if (value < (*tree)->n)
+{
+if ((*tree)->left == NULL)
+{
+(*tree)->left = binary_tree_node(*tree, value);
+return ((*tree)->left);
+}
+return (bst_insert(&((*tree)->left), value));
+}
+if (value > (*tree)->n)
+{
+if ((*tree)->right == NULL)
+{
+(*tree)->right = binary_tree_node(*tree, value);
+return ((*tree)->right);
+}
+return (bst_insert(&(*tree)->right, value));
+}
+return (NULL);
 
 }
