4f0cb55d8a43698bb43381efae486e8c49c8d796
Kaylee Dao <kayleed2@gmail.com>
2022-08-10 12:23:10 -0500

    Remove and reset nodes

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -5,15 +5,15 @@
  * minValueNode - Searches bst for node to remove
  * @node: pointer to BST tree node
  * Return: Pointer to node with value or NULL
- */
+*/
 bst_t *minValueNode(bst_t *node)
 {
-    bst_t *current = node;
+bst_t *current = node;
 
-    while (current && current->left != NULL)
-        current = current->left;
-  
-    return current;
+while (current && current->left != NULL)
+current = current->left;
+
+return (current);
 }
 
 /**
@@ -24,35 +24,37 @@ bst_t *minValueNode(bst_t *node)
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
-    bst_t *temp;
+bst_t *temp;
 
-    if (root == NULL)
-        return (root);
-    
-    if (value < root->n)
-        root->left = bst_remove(root->left, value);
+if (root == NULL)
+return (root);
 
-    else if (value > root->n)
-        root->right = bst_remove(root->right, value);
-  
-    else {
+if (value < root->n)
+root->left = bst_remove(root->left, value);
 
-        if (root->left == NULL) {
-            temp = root->right;
-            free(root);
-            return (temp);
-        }
-        else if (root->right == NULL) {
-            temp = root->left;
-            free(root);
-            return (temp);
-        }
-  
-        temp = minValueNode(root->right);
-  
-        root->n = temp->n;
-  
-        root->right = bst_remove(root->right, temp->n);
-    }
-    return (root);
+else if (value > root->n)
+root->right = bst_remove(root->right, value);
+
+else
+{
+if (root->left == NULL)
+{
+temp = root->right;
+free(root);
+return (temp);
+}
+else if (root->right == NULL)
+{
+temp = root->left;
+free(root);
+return (temp);
+}
+
+temp = minValueNode(root->right);
+
+root->n = temp->n;
+
+root->right = bst_remove(root->right, temp->n);
+}
+return (root);
 }
