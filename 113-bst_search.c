d86ea34459eb58a6bdac04ffbf3a6192b5daabed
Kaylee Dao <kayleed2@gmail.com>
2022-08-10 11:27:37 -0500

    BST Search

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -2,26 +2,24 @@
 #include "limits.h"
 
 /**
- * array_to_bst - builds a Binary Search Tree from an array
+ * bst_search - Searches bst for value
  * @tree: pointer to BST tree node
  * @value: value to search for
  * Return: Pointer to node with value or NULL
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
-    const bst_t *temp = tree;
-    
-    if (temp == NULL)
-    {
-        return (NULL);
-    }
-    if (temp->n == value)
-    {
-        return ((bst_t *)temp);
-    }
-    if (temp->n > value)
-    {
-        return bst_search(temp->left, value);
-    }
-    return bst_search(temp->right, value);
+if (tree == NULL)
+{
+return (NULL);
+}
+if (tree->n == value)
+{
+return ((bst_t *)tree);
+}
+if (tree->n > value)
+{
+return bst_search(tree->left, value);
+}
+return bst_search(tree->right, value);
 }
