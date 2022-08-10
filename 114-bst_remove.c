f693b85bb015425c5812db4233d6be9579ff58a3
Kaylee Dao <kayleed2@gmail.com>
2022-08-10 12:04:23 -0500

    Remove and reset nodes

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -2,6 +2,29 @@
 #include "limits.h"
 
 /**
+ * bst_search - Searches bst for value
+ * @tree: pointer to BST tree node
+ * @value: value to search for
+ * Return: Pointer to node with value or NULL
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+if (tree == NULL)
+{
+return (NULL);
+}
+if (tree->n == value)
+{
+return ((bst_t *)tree);
+}
+if (tree->n > value)
+{
+return (bst_search(tree->left, value));
+}
+return (bst_search(tree->right, value));
+}
+
+/**
  * bst_remove - Searches bst for node to remove
  * @root: pointer to BST tree node
  * @value: value to search for
