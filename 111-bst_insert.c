1d9acd1acdb7eed52fc850799a4b687a076e30f2
Kaylee Dao <kayleed2@gmail.com>
2022-08-10 10:51:01 -0500

    Have to check if either side is NULL

---
+++ 111-bst_insert.c
@@ -0,0 +1,47 @@
+#include "binary_trees.h"
+#include "limits.h"
+
+/**
+ * bst_insert - Inserts value in binary search tree
+ * @tree: double pointer to root node of bst of tree
+ * @value: value to store in inserted *tree
+ * Return: Pointer to new *tree or NULL
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+    bst_t *newnode = binary_tree_node(*tree, value);
+
+     if (tree == NULL)
+    {
+        return (NULL);
+    }
+
+
+    if (*tree == NULL)
+    {
+        *tree = newnode;
+        return (*tree);
+    }
+
+    if (value < (*tree)->n)
+    {
+        if ((*tree)->left == NULL)
+        {
+            (*tree)->left = newnode;
+            return ((*tree)->left);
+        }
+        return (bst_insert(&((*tree)->left), value));
+    }
+    else if (value > (*tree)->n)
+    {
+        if ((*tree)->right == NULL)
+        {
+            (*tree)->right = newnode;
+            return ((*tree)->right);
+        }
+        return (bst_insert(&(*tree)->right, value));
+    }
+ 
+    return (NULL);
+
+}
